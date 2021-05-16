156400e918a7c4c6e33a861c9906b5cc0db60e8d
nikhilgupta <gupta.nikhil@99acres.com>
2021-05-16 23:38:47 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './otherPage';
+import Fib from './fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Link to = "/">Home</Link>
+          <Link to = "/otherage">Other Page</Link>
+          <div>
+            <Route exact path = "/" component = {Fib} />
+            <Route path = "/otherPage" component = {OtherPage} />
+          </div>
+        </header>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
